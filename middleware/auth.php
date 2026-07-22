dcd3887d089412b5f896604e7bb80d67c5903100
yornrotha22-bit <yornrotha22@gmail.com>
2026-07-22 09:45:20 +0700

    Save changes on main

--- middleware/auth.php
+++ middleware/auth.php
@@ -1,15 +1,13 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+// if (session_status() === PHP_SESSION_NONE) {
+//     session_start();
+// }
+// $_SESSION['user_id'] = 1; 
 
-// ដាក់ Fake Session សម្រាប់ Test
-$_SESSION['user_id'] = 1; 
-
-function checkAuth() {
-    if (!isset($_SESSION['user_id'])) {
-        header('Location: /PHP+laravel/PHP/Team5-Clinic/auth/login.php');
-        exit;
-    }
-}
+// function checkAuth() {
+//     if (!isset($_SESSION['user_id'])) {
+//         header('Location: /PHP+laravel/PHP/Team5-Clinic/auth/login.php');
+//         exit;
+//     }
+// }
 ?>
